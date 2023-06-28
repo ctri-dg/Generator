25eb790fafa6da1065a27a26ce9520bae20c56c8
Vishnutha <Vishnutha.Sheela@iiitb.org>
2023-06-28 11:25:08 +0530

    changed generator-main

--- Generator-main/template/server/data-provider/src/main/java/com/example/dataprovider/repositories/Repository.java
+++
@@ -1,7 +0,0 @@
-package com.example.dataprovider.repositories;
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.dataprovider.models.*;
-
-public interface %sRepository extends JpaRepository<%s, %s> {
